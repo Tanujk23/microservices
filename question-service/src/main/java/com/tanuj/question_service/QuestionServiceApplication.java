1e1c058bc5424932e2da2c310e6900439fc71bb6
tanujkaruturi <tanujkaruturi6@gmail.com>
2024-05-29 20:36:43 -0400

    Test

--- question-service/src/main/java/com/tanuj/question_service/QuestionServiceApplication.java
+++ question-service/src/main/java/com/tanuj/question_service/QuestionServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Tes");
 		SpringApplication.run(QuestionServiceApplication.class, args);
 	}
 
